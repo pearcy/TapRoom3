28012aff13361aa2949383782978824ceb9101a8
ppearce05 <ppearce05@Josephs-Mac.local>
2020-09-19 08:06:42 -0700

    fix Keg Details in KegControl

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -64,8 +64,8 @@ class KegControl extends React.Component {
           });
       }
 
-      handleChangingSelectedKeg = (id) => {
-          const selectedKeg = this.state.masterKegList[id];
+      handleSelectedKeg = (id) => {
+          const selectedKeg = this.props.masterKegList[id];
           this.setState({selectedKeg: selectedKeg
         });
       }
@@ -115,7 +115,7 @@ class KegControl extends React.Component {
                 } else {
                 currentlyVisibleState = <KegList 
                 kegList={this.props.masterKegList} 
-                onKegSelection={this.handleChangingSelectedKeg}
+                onKegSelection={this.handleSelectedKeg}
                  />;
                 buttonText = "Add Keg" 
                 }
